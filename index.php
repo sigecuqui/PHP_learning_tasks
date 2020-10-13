19fbfd1bdeb32d423b64b051788f23b4075f6b72
sigecuqui <sigita.guogaite@gmail.com>
2020-10-13 09:45:39 +0300

    pataisyta

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Aš valdau HTML!</title>
 </head>
 <body>
-<h1>Vardas - HTML asas/ė!</h1>
+<h1 style="font-weight: lighter"><b style="strong italic">Vardas</b> - HTML asas/ė!</h1>
 <p>Moku kurti statinį turinį.</p>
 </body>
 </html>
