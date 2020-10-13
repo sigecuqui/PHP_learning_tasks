e9fdd9f28b6d2b5fae82fae445f777df0de9e5e4
sigecuqui <sigita.guogaite@gmail.com>
2020-10-13 10:01:38 +0300

    with php

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Aš valdau HTML!</title>
+    <title>Aš valdau HTML <?php print 'ir PHP'?>!</title>
 </head>
 <body>
-<h1 style="font-weight: lighter"><b style="strong">Vardas</b> - HTML asas/ė!</h1>
-<p>Moku kurti statinį turinį.</p>
+<h1 style="font-weight: lighter"><b>Vardas</b> - HTML <?php print 'ir PHP'?> asas/ė!</h1>
+<p>Moku kurti statinį turinį.
+    Atidaryta: <?php print 'Y-m-d H:i:s'?>
+</p>
 </body>
 </html>
