e0104e32f96d5b55f68b591e76639aa3cc753696
sigecuqui <sigita.guogaite@gmail.com>
2020-10-14 12:42:32 +0300

    fuel task my money taisyta

--- index.php
+++ index.php
@@ -3,15 +3,18 @@
 $distance = rand(200, 1000);
 $consumption = 7.5;
 $price_1 = 1.3;
+$my_money = 100;
+
 $fuel_total = $distance * $consumption / 100;
 $price_trip = $fuel_total * $price_1;
-$trip_cost = number_format($price_trip, 2);
-$my_money = 100;
+
 if ($my_money >= $price_trip) {
     $available_trip = 'įperkama';
 } else {
     $available_trip = 'neįperkama';
 }
+
+$trip_cost = number_format($price_trip, 2);
 ?>
 <!DOCTYPE html>
 <html lang="en">
