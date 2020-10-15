bebece152ea411157d50afdb148c61d4ed6709f1
sigecuqui <sigita.guogaite@gmail.com>
2020-10-15 16:28:39 +0300

    kariuomene

--- index.php
+++ index.php
@@ -1,48 +1,57 @@
+<?php date_default_timezone_set('Europe/Vilnius'); ?>
 <?php
-$minutes = abs(date('i') - 59);
-$minutes_remain = $minutes % 5;
-$seconds = abs(date('s') - 59);
+$name = 'Kebabas';
+$age = rand(13, 69);
+$conviction = rand(0, 1);
+$children = rand(1, 5);
+$reason_children = '';
+$reason_conviction = '';
+$reason_age = '';
+if ($conviction) {
+    $convicted_person = 'teistas';
+} else {
+    $convicted_person = 'neteistas';
+}
+if ($age >= 18 && $age <= 25 && !$conviction && $children <= 2) {
+    $h2 = $name . ' yra šaukiamas į kariuomenę!';
+} else {
+    $h1 = $name . ' nėra šaukiamas į kariuomenę, nes';
+    if ($children > 2) {
+        $reason_children = ' turi ' . $children . ' vaikus';
+    }
+    if ($conviction) {
+        $reason_conviction = ' yra ' . $convicted_person;
+    }
+    if ($age < 18) {
+        $reason_age = ' per jaunas';
+    }
+    if ($age > 25) {
+        $reason_age = ' per senas';
+    }
+    $h2 = $h1 . $reason_children . $reason_conviction . $reason_age;
+}
 ?>
 <!DOCTYPE html>
-<html lang='en'>
+<html lang="en">
 <head>
-    <meta charset='utf-8'>
-    <title>Joint’o Load</title>
+    <meta charset="utf-8">
+    <title>Military</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
 </head>
-<style>
-    body {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100vH;
-    }
-
-    .joint-smoked {
-        background-image: url('./burned.png');
-        background-size: cover;
-        width: <?php print $minutes_remain.$seconds/100; ?>%;
-        height: 400px;
-        display: flex;
-        justify-content: center;
-        position: absolute;
-        left: 0;
-    }
-
-    .joint {
-        position: relative;
-        background-image: url('./joint.png');
-        background-size: cover;
-        width: 400px;
-        height: 400px;
-        display: flex;
-        justify-content: center;
-    }
-</style>
 <body>
-<div class='joint'>
-    <div class='joint-smoked'></div>
-</div>
-<p>Liko: <?php print $minutes_remain . ':' . $seconds; ?> </p>
+<ul>
+    <li>Vardas: <?php print $name; ?></li>
+    <li>Amžius: <?php print $age; ?></li>
+    <li>Teistumas: <?php print $convicted_person; ?></li>
+    <li>Turi vaikų: <?php print $children; ?></li>
+</ul>
+<h2><?php print $h2; ?></h2>
 </body>
 </html>
