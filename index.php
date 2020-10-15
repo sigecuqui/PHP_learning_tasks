0febf0be29caa2b257aa13584d64bdbb781fc7a7
sigecuqui <sigita.guogaite@gmail.com>
2020-10-15 16:59:37 +0300

    keliamieji metai

--- index.php
+++ index.php
@@ -1,41 +1,19 @@
 <?php date_default_timezone_set('Europe/Vilnius'); ?>
 <?php
-$name = 'Kebabas';
-$age = rand(13, 69);
-$conviction = rand(0, 1);
-$children = rand(1, 5);
-$reason_children = '';
-$reason_conviction = '';
-$reason_age = '';
-if ($conviction) {
-    $convicted_person = 'teistas';
+$years = rand(1991, 2020);
+
+if ($years % 400 === 0 || $years % 4 === 0 && $years % 100 !== 0) {
+    $type = 'Keliamieji';
 } else {
-    $convicted_person = 'neteistas';
+    $type = 'Nekeliamieji';
 }
-if ($age >= 18 && $age <= 25 && !$conviction && $children <= 2) {
-    $h2 = $name . ' yra šaukiamas į kariuomenę!';
-} else {
-    $h1 = $name . ' nėra šaukiamas į kariuomenę, nes';
-    if ($children > 2) {
-        $reason_children = ' turi ' . $children . ' vaikus';
-    }
-    if ($conviction) {
-        $reason_conviction = ' yra ' . $convicted_person;
-    }
-    if ($age < 18) {
-        $reason_age = ' per jaunas';
-    }
-    if ($age > 25) {
-        $reason_age = ' per senas';
-    }
-    $h2 = $h1 . $reason_children . $reason_conviction . $reason_age;
-}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Military</title>
+    <title>Keliamieji metai</title>
     <style>
         body {
             display: flex;
@@ -46,12 +24,8 @@ if ($age >= 18 && $age <= 25 && !$conviction && $children <= 2) {
     </style>
 </head>
 <body>
-<ul>
-    <li>Vardas: <?php print $name; ?></li>
-    <li>Amžius: <?php print $age; ?></li>
-    <li>Teistumas: <?php print $convicted_person; ?></li>
-    <li>Turi vaikų: <?php print $children; ?></li>
-</ul>
-<h2><?php print $h2; ?></h2>
+<h1>Ar metai yra keliamieji?</h1>
+<p>Dabar yra: <?php print $years ;?> metai</p>
+<h2>Šie metai yra: <?php print $type; ?></h2>
 </body>
 </html>
