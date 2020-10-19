d96968e8a5ecb77ea7a8d79f8fdb69c3e8a5f538
sigecuqui <sigita.guogaite@gmail.com>
2020-10-19 15:38:30 +0300

    for cycle stars multiplication

--- index.php
+++ index.php
@@ -1,28 +1,31 @@
 <?php
-$stars = rand(1, 30);
-
-for ($i = 0; $i <= $stars; $i++) {
-    for ($j = 1; $j <= $i; $j++) {
-        print '* ';
-    }
-    print '<br>';
-}
-
-for ($i = 0; $i <= $stars; $i++) {
-    for ($j = $stars - $i; $j >= 1; $j--) {
-        echo '* ';
-    }
-    echo '<br>';
-}
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Triangle</title>
+    <title>Multiplication</title>
+    <style>
+        body {
+            display: grid;
+            grid-template-columns: repeat(9, 1fr);
+            grid-gap: 15px;
+        }
+
+        .container {
+            border: 1px solid black;
+            padding: 10px;
+        }
+    </style>
 </head>
 <body>
-
+<?php for ($i = 1; $i <= 9; $i++): ?>
+    <div class="container">
+        <?php for ($j = 1; $j <= 9; $j++) :
+            print "$i * $j = " . $i * $j . "</br>"; ?>
+        <?php endfor; ?>
+    </div>
+<?php endfor; ?>
 </body>
 </html>
