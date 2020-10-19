af4db2a3e25066c367826c382e565ecb4a1b59e5
sigecuqui <sigita.guogaite@gmail.com>
2020-10-19 16:09:10 +0300

    for cycle stars checker board

--- index.php
+++ index.php
@@ -5,27 +5,51 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Multiplication</title>
+    <title>Checker board</title>
     <style>
         body {
-            display: grid;
-            grid-template-columns: repeat(9, 1fr);
-            grid-gap: 15px;
+            display: flex;
+            justify-content: center;
+            background: pink;
         }
 
-        .container {
-            border: 1px solid black;
-            padding: 10px;
+        .white {
+            background-color: white;
+            width: 30px;
+            height: 30px;
+            margin: 0;
         }
+
+        .black {
+            background-color: black;
+            width: 30px;
+            height: 30px;
+            margin: 0;
+        }
+
     </style>
 </head>
 <body>
-<?php for ($i = 1; $i <= 9; $i++): ?>
-    <div class="container">
-        <?php for ($j = 1; $j <= 9; $j++) :
-            print "$i * $j = " . $i * $j . "</br>"; ?>
-        <?php endfor; ?>
-    </div>
-<?php endfor; ?>
+<?php
+for ($i = 0; $i < 8; $i++) {
+    print '<div class="container">';
+    for ($j = 0; $j < 8; $j++) {
+        if ($i % 2 == 0) {
+            if ($j % 2 == 0) {
+                print '<div class="white"></div>';
+            } else {
+                print '<div class="black"></div>';
+            }
+        } else {
+            if ($j % 2 == 0) {
+                print '<div class="black"></div>';
+            } else {
+                print '<div class="white"></div>';
+            }
+        }
+    }
+    print "</div>";
+}
+?>
 </body>
 </html>
