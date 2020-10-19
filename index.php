e131ef04a8c15fce909020a607f20262890c60e4
sigecuqui <sigita.guogaite@gmail.com>
2020-10-19 11:03:08 +0300

    for cycle 3 whith months

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-$x = rand(1, 10);
-
-for ($i = 1; $i <= $x; $i++) {
-    var_dump("Tai yra . $i . -tasis ciklas");
+for ($i = 1; $i < 12; $i++) {
+    var_dump(date('F', strtotime("+$i month")));
 }
