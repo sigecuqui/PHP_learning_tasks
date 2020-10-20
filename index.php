d86f7580bed1c70c8b62d0625ae4338d88ff3819
sigecuqui <sigita.guogaite@gmail.com>
2020-10-20 11:39:07 +0300

    for cycle rukas wasted time

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $days = 365;
 $pack_price = 3.5;
 $count_ttl = 0;
 $cigs_workdays = 0;
-
+$time_per_cige = 5;
 for ($i = 0; $i <= $days; $i++) {
     $day = date('N', strtotime("+ $i day"));
     if ($day <= 5) {
@@ -21,9 +21,11 @@ for ($i = 0; $i <= $days; $i++) {
 
 $price_ttl = ceil($count_ttl / 20) * $pack_price;
 $total_mon_fri = ceil($cigs_workdays / 20) * $pack_price;
+$time_total = ceil($time_per_cige * $count_ttl / 60);
 
 $h2 = "Per $days dienas surūkysiu $count_ttl cigarečių už $price_ttl eur.";
 $h3 = "Nerūkydamas darbo dienomis sutaupyčiau $total_mon_fri eur";
+$wasted_time = "Viso traukdamas prastovėsiu $time_total valandų"
 ?>
 <?php date_default_timezone_set('Europe/Vilnius'); ?>
 <!doctype html>
@@ -36,4 +38,5 @@ $h3 = "Nerūkydamas darbo dienomis sutaupyčiau $total_mon_fri eur";
 <h1>Mano dūmų skaičiuoklė</h1>
 <h2><?php print $h2; ?></h2>
 <h3><?php print $h3; ?></h3>
+<h3><?php print $wasted_time; ?></h3>
 </body>
