3b408ca4f4a3098ca7c68ae4ce9be61bcc821d84
sigecuqui <sigita.guogaite@gmail.com>
2020-10-20 20:19:45 +0300

    for cycle depozito

--- index.php
+++ index.php
@@ -1,23 +1,34 @@
 <?php
-$speed_of_sound = 333;
-$max_db = 120;
-$sec = 60;
-$db = 120;
+$pabiruciai = rand(3, 8);
+$suaugeliai = rand(4, 10);
+$kids_butelkos_kaina = 0.65;
+$adults_butelkos_kaina = 1.15;
+$juice_quantity = 0;
+$beer_quantity = 0;
+$deposito = 0.1;
+for ($k = 1; $k <= $pabiruciai; $k++) {
+    $juice_bottle = rand(1, 5);
+    $juice_quantity += $juice_bottle;
+    $juice_total_price = $juice_quantity * $kids_butelkos_kaina;
+}
+for ($a = 1; $a <= $suaugeliai; $a++) {
+    $beer_bottle = rand(1, 5);
+    $beer_quantity += $beer_bottle;
+    $beer_total_price = $beer_quantity * $adults_butelkos_kaina;
+}
+$depozitas = ($beer_quantity + $juice_quantity) * $deposito;
+$h2 = "$pabiruciai vaikų išgėrė $juice_quantity buteliukų; bendra suma $juice_total_price eur";
+$h2_adult = "$suaugeliai suaugusiųjų išgėrė $beer_quantity buteliukų; bendra suma $beer_total_price eur, atgausiu depozito: $deposito eur";
 ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>speedo</title>
+    <meta charset="utf-8">
+    <title>Depozito</title>
 </head>
 <body>
-<h1>Griaustinio zona</h1>
-<article><?php for ($i = 1; $i <= $sec; $i++): ?>
-        <?php $dist = $speed_of_sound * $i; ?>
-        <?php for ($j = 1; $j <= $speed_of_sound * $i / 10; $j++): ?>
-            <?php $db -= $db / 100 * 25; ?>
-        <?php endfor; ?>
-        <p><?php print "Po $i sec. ($dist m.): $db" ?></p>
-    <?php endfor; ?>
-</article>
+<h2><?php print $h2; ?></h2>
+<h2><?php print $h2_adult; ?></h2>
+
 </body>
 </html>
