a45c95f8aa7e98220360927550952c0f0722e2d6
sigecuqui <sigita.guogaite@gmail.com>
2020-10-20 10:50:22 +0300

    for cycle rukas

--- index.php
+++ index.php
@@ -1,65 +1,36 @@
 <?php
-$table_height = 5;
-$table_width = 3;
+$days = 365;
+$pack_price = 3.5;
+$count_ttl = 0;
+$price_ttl = 0;
+$cigs_sun = rand(1, 3);
+$cigs_sat = rand(10, 20);
+$cigs_mon_fri = rand(3, 4);
+
+for ($i = 0; $i <= $days; $i++) {
+    $day = date('N', strtotime("+ $i day"));
+    if ($day <= 5) {
+        $count_ttl += $cigs_mon_fri;
+    }
+    elseif ($day == 6) {
+        $count_ttl += $cigs_sat;
+    } else {
+        $count_ttl += $cigs_sun;
+    }
+}
+
+$price_ttl = $count_ttl * $pack_price / 20;
+$isvada = "Per $days dienas surūkysiu $count_ttl cigarečių už $price_ttl eur.";
+
 ?>
+<?php date_default_timezone_set('Europe/Vilnius'); ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Alphabet</title>
-    <style>
-        body {
-            text-align: center;
-        }
-        .container {
-            margin: 50px auto;
-            width: 400px;
-        }
-        .letter {
-            border: 1px solid gray;
-            margin: 10px;
-            padding: 10px;
-            line-height: 10px;
-            display: inline-block;
-        }
-        .letter .empty {
-            opacity: 0.1;
-        }
-    </style>
+    <title>Rūkas</title>
 </head>
 <body>
-<div class="container">
-    <div class="letter a">
-        <?php
-        for ($x = 1; $x <= $table_height; $x++) {
-            for ($y = 1; $y <= $table_width; $y++) {
-                if ($x == 1 && $y == 2) {
-                    print ' • ';
-                } elseif ($x == 1 && ($y == 1 || $y == 3)) {
-                    print '<span class="empty"> • </span>';
-                }
-                if ($x == 2 && ($y == 1 || $y == 3)) {
-                    print ' • ';
-                } elseif ($x == 2 && $y == 2) {
-                    print '<span class="empty"> • </span>';
-                }
-                if ($x == 3 && ($y == 1 || $y == 2 || $y == 3)) {
-                    print ' • ';
-                } elseif ($x == 3 && ($y == 1 || $y == 3)) {
-                    print '<span class="empty"> • </span>';
-                }
-                if ($x == 4 && ($y == 1 || $y == 3)) {
-                    print ' • ';
-                } elseif ($x == 4 && $y == 2) {
-                    print '<span class="empty"> • </span>';
-                }
-                if ($x == 5 && ($y == 1 || $y == 3)) {
-                    print ' • ';
-                } elseif ($x == 5 && $y == 2) {
-                    print '<span class="empty"> • </span>';
-                }
-            }
-            print '</br>';
-        }
-        ?>
-    </div>
+<h1>Mano dūmų skaičiuoklė</h1>
+<h2><?php print $isvada; ?></h2>
+</body>
