cf531f252a36dc30752ebe77b2b57c6c7faeebea
sigecuqui <sigita.guogaite@gmail.com>
2020-10-20 12:51:29 +0300

    for cycle rukas cigaretes ekrane

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ for ($i = 0; $i <= $days; $i++) {
         $count_ttl += $cigs_sun;
     }
 }
-
+$packages = ceil($count_ttl / 20);
 $price_ttl = ceil($count_ttl / 20) * $pack_price;
 $total_mon_fri = ceil($cigs_workdays / 20) * $pack_price;
 $time_total = ceil($time_per_cige * $count_ttl / 60);
@@ -34,9 +34,32 @@ $wasted_time = "Viso traukdamas prastovėsiu $time_total valandų"
     <meta charset="UTF-8">
     <title>Rūkas</title>
 </head>
+<style>
+    .pack {
+        border: 1px solid black;
+        margin: 20px;
+        display: grid;
+        grid-template-columns: repeat(10, 1fr);
+    }
+
+    .cige {
+        background-image: url("https://www.freeiconspng.com/thumbs/cigarettes-png/cigarettes-png-0.png");
+        background-size: cover;
+        width: 100px;
+        height: 100px;
+    }
+</style>
 <body>
 <h1>Mano dūmų skaičiuoklė</h1>
 <h2><?php print $h2; ?></h2>
 <h3><?php print $h3; ?></h3>
 <h3><?php print $wasted_time; ?></h3>
+<?php for ($i = 0; $i < $packages; $i++): ?>
+    <div class="pack">
+        <?php for ($y = 0; ($y < 20 && $count_ttl); $y++, $count_ttl--): ?>
+            <div class="cige"></div>
+        <?php endfor; ?>
+    </div>
+<?php endfor; ?>
 </body>
+</html>
