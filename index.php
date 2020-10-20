236a400eefb19dcda0e2b357392e2bf0eca14ca4
sigecuqui <sigita.guogaite@gmail.com>
2020-10-20 11:17:11 +0300

    for cycle rukas with ceil minus workdays

--- index.php
+++ index.php
@@ -2,26 +2,28 @@
 $days = 365;
 $pack_price = 3.5;
 $count_ttl = 0;
-$price_ttl = 0;
-$cigs_sun = rand(1, 3);
-$cigs_sat = rand(10, 20);
-$cigs_mon_fri = rand(3, 4);
+$cigs_workdays = 0;
 
 for ($i = 0; $i <= $days; $i++) {
     $day = date('N', strtotime("+ $i day"));
     if ($day <= 5) {
+        $cigs_mon_fri = rand(3, 4);
         $count_ttl += $cigs_mon_fri;
-    }
-    elseif ($day == 6) {
+        $cigs_workdays += $cigs_mon_fri;
+    } elseif ($day == 6) {
+        $cigs_sat = rand(10, 20);
         $count_ttl += $cigs_sat;
     } else {
+        $cigs_sun = rand(1, 3);
         $count_ttl += $cigs_sun;
     }
 }
 
-$price_ttl = $count_ttl * ceil($pack_price / 20);
-$isvada = "Per $days dienas surūkysiu $count_ttl cigarečių už $price_ttl eur.";
+$price_ttl = ceil($count_ttl / 20) * $pack_price;
+$total_mon_fri = ceil($cigs_workdays / 20) * $pack_price;
 
+$h2 = "Per $days dienas surūkysiu $count_ttl cigarečių už $price_ttl eur.";
+$h3 = "Nerūkydamas darbo dienomis sutaupyčiau $total_mon_fri eur";
 ?>
 <?php date_default_timezone_set('Europe/Vilnius'); ?>
 <!doctype html>
@@ -32,5 +34,6 @@ $isvada = "Per $days dienas surūkysiu $count_ttl cigarečių už $price_ttl eur
 </head>
 <body>
 <h1>Mano dūmų skaičiuoklė</h1>
-<h2><?php print $isvada; ?></h2>
+<h2><?php print $h2; ?></h2>
+<h3><?php print $h3; ?></h3>
 </body>
