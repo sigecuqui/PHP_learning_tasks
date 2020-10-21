66611c3b06626936883f51046bea643707cf6f17
sigecuqui <sigita.guogaite@gmail.com>
2020-10-21 16:31:33 +0300

    leaderboard

--- index.php
+++ index.php
@@ -1,39 +1,54 @@
 <?php
-$hours = 12;
-$ticket_price = 13;
-$money_earned = 0;
-$total_people = 0;
-$total_people_exit = 0;
-$carousel_spin = 0;
-
-for ($x = 0; $x < $hours; $x++) {
-    $visitors = rand(1, 20);
-    if ($visitors < 10) {
-        $total_people_exit += $visitors;
-    } else {
-        $total_people_exit += $visitors - 10;
-        $carousel_spin++;
-        $total_people += 10;
+$score_max = 100;
+for ($i = 1; $i <= 10; ++$i) {
+    $score = rand($score_max - 10, $score_max);
+    switch (rand(0, 9)) {
+        case 0:
+            $name = "Joaquin Spence";
+            break;
+        case 1:
+            $name = "Chris Velasquez";
+            break;
+        case 2:
+            $name = "Jesse Moreno";
+            break;
+        case 3:
+            $name = "Mylee Simmons";
+            break;
+        case 4:
+            $name = "Allison Barry";
+            break;
+        case 5:
+            $name = "Aubree Irwin";
+            break;
+        case 6:
+            $name = "Kobe Nguyen";
+            break;
+        case 7:
+            $name = "Reuben Howard";
+            break;
+        case 8:
+            $name = "Rocco Livingston";
+            break;
+        case 9:
+            $name = "Tyrone Dodson";
+            break;
     }
+    print $i . ' ' . $name . ' ' . $score . '</br>';
+    $score_max -= 10;
 }
-
-$money_earned += $ticket_price * $total_people;
-
-$h2 = "karusėlė uždirbo $money_earned eur";
-$h3 = "Žmonių prasisuko: $total_people";
-$h4 = "Žmonių išėjo: $total_people_exit";
-$h5 = "Karuselė sukosi $carousel_spin kartu";
 ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Carousel</title>
+    <title>Leaderboard</title>
+    <style>
+        body {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-<h2><?php print $h2 ?></h2>
-<h3><?php print $h3 ?></h3>
-<h4><?php print $h4 ?></h4>
-<h5><?php print $h5 ?></h5>
 </body>
 </html>
