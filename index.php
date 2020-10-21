841e619da4e745166e0b121d5e3cd5a21b40cf32
sigecuqui <sigita.guogaite@gmail.com>
2020-10-21 09:15:00 +0300

    for cycle spalvos

--- index.php
+++ index.php
@@ -1,34 +1,23 @@
 <?php
-$week_gain = 0;
-$week_lost = 0;
-
-for ($c = 1; $c <= 7; $c++) {
-    $day_calories_gain = rand(1300, 3000);
-    $day_calories_lost = rand(1000, 3200);
-    $week_gain += $day_calories_gain;
-    $week_lost += $day_calories_lost;
-    $week_calories = $week_gain - $week_lost;
-    if ($week_gain > $week_lost) {
-        $h2 = 'Svoris auga, beib';
-    }
-    elseif ($week_gain < $week_lost) {
-        $h2 = 'Svoris krenta, beib';
-    } else {
-        $h2 = 'Stabilus riebaliukas';
-    }
-}
-
-$week = "Per savaitėlę pririjai $week_gain kalorijų, bet numetei $week_lost";
-
+$shades = 15;
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Riebaliukai</title>
+    <meta charset="UTF-8">
+    <title> Spalvos </title>
+    <style>
+        .block {
+            width: 300px;
+            height: 25px;
+        }
+    </style>
 </head>
 <body>
-<h2><?php print $week; ?></h2>
-<h2><?php print $h2; ?></h2>
+<?php for ($x = 0; $x < $shades; $x++) : ?>
+    <div class="block"
+         style="background-color: rgb(<?php print 255 * $x / $shades .', '; ?> 0, 0)"></div>
+<?php endfor; ?>
+<div></div>
 </body>
 </html>
