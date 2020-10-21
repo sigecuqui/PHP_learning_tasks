1ef6a42cbd499cb843af5a96b9ebe4b212b8e304
sigecuqui <sigita.guogaite@gmail.com>
2020-10-21 14:50:42 +0300

    for cycle death tree

--- index.php
+++ index.php
@@ -1,20 +1,38 @@
 <?php
-$route = [
-    90,
-    -30,
-    60,
-    -150,
-    20
-];
+$height = rand(1, 30);
 
-var_dump($route);
+for ($a = 1; $a <= $height; $a++) {
+    for ($b = 1; $b <= $a; $b++) {
+        $decorations = rand(1, 30);
+        if ($decorations == 1) {
+            print '🔪';
+        } elseif ($decorations == 2) {
+            print '😭';
+        } elseif ($decorations == 3) {
+            print '⚔️';
+        } else {
+            print '☠ ';
+        }
+    }
+    print "<br>";
+}
+for ($a = 1; $a <= 3; $a++) {
+    for ($b = 1; $b <= 2; $b++) {
+        print "☢ ";
+    }
+    print "<br>";
+}
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Array</title>
+    <title>Death Tree</title>
+    <style>
+        body {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-
 </body>
 </html>
