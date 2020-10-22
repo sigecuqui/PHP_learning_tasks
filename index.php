38ca446810313a64bf58590e3d44d9e2b7f64a26
sigecuqui <sigita.guogaite@gmail.com>
2020-10-22 11:00:59 +0300

    kvadratas 1

--- index.php
+++ index.php
@@ -1,53 +1,25 @@
 <?php
-$score_max = 100;
-for ($i = 1; $i <= 10; ++$i) {
-    $score = rand($score_max - 10, $score_max);
-    switch (rand(0, 9)) {
-        case 0:
-            $name = "Joaquin Spence";
-            break;
-        case 1:
-            $name = "Chris Velasquez";
-            break;
-        case 2:
-            $name = "Jesse Moreno";
-            break;
-        case 3:
-            $name = "Mylee Simmons";
-            break;
-        case 4:
-            $name = "Allison Barry";
-            break;
-        case 5:
-            $name = "Aubree Irwin";
-            break;
-        case 6:
-            $name = "Kobe Nguyen";
-            break;
-        case 7:
-            $name = "Reuben Howard";
-            break;
-        case 8:
-            $name = "Rocco Livingston";
-            break;
-        case 9:
-            $name = "Tyrone Dodson";
-            break;
-    }
-    print $i . ' ' . $name . ' ' . $score . '</br>';
-    $score_max -= 10;
-}
+$green = 0;
+$yellow = 1;
+$red = 2;
+
+$kvadrat = [
+    [$green, $green, $green, $green, $green, $green, $green, $green],
+    [$green, $yellow, $yellow, $yellow, $yellow, $yellow, $yellow, $green],
+    [$green, $yellow, $red, $red, $red, $red, $yellow, $green],
+    [$green, $yellow, $red, $yellow, $yellow, $red, $yellow, $green],
+    [$green, $yellow, $red, $yellow, $yellow, $red, $yellow, $green],
+    [$green, $yellow, $red, $red, $red, $red, $yellow, $green],
+    [$green, $yellow, $yellow, $yellow, $yellow, $yellow, $yellow, $green],
+    [$green, $green, $green, $green, $green, $green, $green, $green]
+];
+var_dump($kvadrat);
 ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Leaderboard</title>
-    <style>
-        body {
-            text-align: center;
-        }
-    </style>
+    <title>Spalvu kvadratas</title>
 </head>
 <body>
 </body>
