05bfb75084938dd294dad875d2eb851f97fa0814
sigecuqui <sigita.guogaite@gmail.com>
2020-10-22 12:18:18 +0300

    memories foreach

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ $my_memories = [
     'Išpirkimas'
 ];
 
+$flashback_index = rand(0, count($my_memories) - 1);
+$flashback_text = $my_memories[$flashback_index];
+$h3 = "Flashback $flashback_index : $flashback_text";
 ?>
 <!doctype html>
 <html lang="en">
@@ -24,5 +27,6 @@ $my_memories = [
         <li><?php print $memory; ?></li>
     <?php endforeach; ?>
 </ul>
+<h3><?php print $h3; ?></h3>
 </body>
 </html>
