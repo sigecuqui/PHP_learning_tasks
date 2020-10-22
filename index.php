bd12bf26c31fbee69a94ac4548319bc9a7957ebb
sigecuqui <sigita.guogaite@gmail.com>
2020-10-22 11:06:15 +0300

    kvadratas 2 su konstanta

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
-$green = 0;
-$yellow = 1;
-$red = 2;
+define('G', 0);
+define('Y', 1);
+define('R', 2);
 
 $kvadrat = [
-    [$green, $green, $green, $green, $green, $green, $green, $green],
-    [$green, $yellow, $yellow, $yellow, $yellow, $yellow, $yellow, $green],
-    [$green, $yellow, $red, $red, $red, $red, $yellow, $green],
-    [$green, $yellow, $red, $yellow, $yellow, $red, $yellow, $green],
-    [$green, $yellow, $red, $yellow, $yellow, $red, $yellow, $green],
-    [$green, $yellow, $red, $red, $red, $red, $yellow, $green],
-    [$green, $yellow, $yellow, $yellow, $yellow, $yellow, $yellow, $green],
-    [$green, $green, $green, $green, $green, $green, $green, $green]
+    [G, G, G, G, G, G, G, G],
+    [G, Y, Y, Y, Y, Y, Y, G],
+    [G, Y, R, R, R, R, Y, G],
+    [G, Y, R, Y, Y, R, Y, G],
+    [G, Y, R, Y, Y, R, Y, G],
+    [G, Y, R, R, R, R, Y, G],
+    [G, Y, Y, Y, Y, Y, Y, G],
+    [G, G, G, G, G, G, G, G]
 ];
 var_dump($kvadrat);
 ?>
